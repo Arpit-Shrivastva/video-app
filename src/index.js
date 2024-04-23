cf0c315ab5a277924e9d1a9357ad79ccbd9ff3f9
Arpit Shrivastava <arpitshri21@gmail.com>
2024-04-23 21:12:13 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+// import('dotenv').config({ path: './env' })
+import dotenv from 'dotenv'
+import connectDB from './db/index.js'
+import { app } from './app.js'
+
+
+dotenv.config({
+    path: './.env'
+})
+
+//SECOND APPROACH 
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port : ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("Mongo DB connection failed !!! ", err);
+    })
+
+
+/*
+
+FIRST APPROACH
+
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import express from "express";
+const app = express();
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log('Error while connecting to the mongoDB', error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error);
+        throw err
+    }
+})()
+*/
