294f47f55bc3f85c035eeaa760df778e21afa575
Arpit Shrivastava <152637028+Arpit-Shrivastva@users.noreply.github.com>
2024-10-22 17:31:00 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -104,6 +104,7 @@ const registerUser = asyncHandler(async (req, res) => {
 })
 
 const loginUser = asyncHandler(async (req, res) => {
+    
     // req body - data
     // username or email
     // find the user
